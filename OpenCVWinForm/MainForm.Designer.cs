28d9554ee675845083e3b7ae97a4881dd2e18b4c
Pravin Bendre <pravinbendre772@gmail.com>
2018-12-08 20:11:30 +1100

    Done

---
+++ OpenCVWinForm/MainForm.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace OpenCVWinForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSaveImage = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pctCvWindow = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pctLeftEye = new System.Windows.Forms.PictureBox();
+            this.pctRightEye = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCvWindow)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLeftEye)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRightEye)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSaveImage);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 584);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(700, 37);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSaveImage
+            // 
+            this.btnSaveImage.Location = new System.Drawing.Point(84, 7);
+            this.btnSaveImage.Name = "btnSaveImage";
+            this.btnSaveImage.Size = new System.Drawing.Size(95, 23);
+            this.btnSaveImage.TabIndex = 1;
+            this.btnSaveImage.Text = "Save Image";
+            this.btnSaveImage.UseVisualStyleBackColor = true;
+            this.btnSaveImage.Click += new System.EventHandler(this.btnSaveImage_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(3, 7);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "jpg";
+            this.saveFileDialog1.Filter = "JPEG|*.jpg";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(700, 584);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.pctCvWindow);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(700, 449);
+            this.panel4.TabIndex = 2;
+            // 
+            // pctCvWindow
+            // 
+            this.pctCvWindow.Location = new System.Drawing.Point(0, 1);
+            this.pctCvWindow.Name = "pctCvWindow";
+            this.pctCvWindow.Size = new System.Drawing.Size(697, 428);
+            this.pctCvWindow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctCvWindow.TabIndex = 0;
+            this.pctCvWindow.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.splitContainer1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 449);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(700, 135);
+            this.panel3.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pctLeftEye);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pctRightEye);
+            this.splitContainer1.Size = new System.Drawing.Size(700, 135);
+            this.splitContainer1.SplitterDistance = 350;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pctLeftEye
+            // 
+            this.pctLeftEye.Location = new System.Drawing.Point(0, -15);
+            this.pctLeftEye.Name = "pctLeftEye";
+            this.pctLeftEye.Size = new System.Drawing.Size(347, 150);
+            this.pctLeftEye.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctLeftEye.TabIndex = 0;
+            this.pctLeftEye.TabStop = false;
+            this.pctLeftEye.Click += new System.EventHandler(this.pctLeftEye_Click);
+            // 
+            // pctRightEye
+            // 
+            this.pctRightEye.Location = new System.Drawing.Point(0, -15);
+            this.pctRightEye.Name = "pctRightEye";
+            this.pctRightEye.Size = new System.Drawing.Size(343, 150);
+            this.pctRightEye.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctRightEye.TabIndex = 1;
+            this.pctRightEye.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 621);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OpenCV";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pctCvWindow)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pctLeftEye)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctRightEye)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnSaveImage;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pctCvWindow;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pctLeftEye;
+        private System.Windows.Forms.PictureBox pctRightEye;
+    }
+}
+
---
+++ OpenCVWinForm/MainForm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+using OpenCvSharp;
+
+namespace OpenCVWinForm
+{
+    public partial class MainForm : Form
+    {
+        private Thread _cameraThread;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        #region Camera Thread
+        private void CaptureCamera()
+        {
+            _cameraThread = new Thread(new ThreadStart(CaptureCameraCallback));
+            _cameraThread.Start();
+        }
+        
+        private void CaptureCameraCallback()
+        {
+            const double ScaleFactor = 2.5;
+            const int MinNeighbors = 1;
+            CvSize MinSize = new CvSize(30, 30);
+
+            CvCapture cap = CvCapture.FromCamera(1);
+            CvHaarClassifierCascade cascade = CvHaarClassifierCascade.FromFile("haarcascade_eye.xml");
+            while (true)
+            {
+                IplImage img = cap.QueryFrame();
+                CvSeq<CvAvgComp> eyes = Cv.HaarDetectObjects(img, cascade, Cv.CreateMemStorage(), ScaleFactor, MinNeighbors, HaarDetectionType.DoCannyPruning, MinSize);
+
+                foreach (CvAvgComp eye in eyes.AsParallel())
+                {
+                    img.DrawRect(eye.Rect, CvColor.Red);
+
+                    if (eye.Rect.Left > pctCvWindow.Width / 2)
+                    {
+                        try
+                        {
+                            IplImage rightEyeImg1 = img.Clone();
+                            Cv.SetImageROI(rightEyeImg1, eye.Rect);
+                            IplImage rightEyeImg2 = Cv.CreateImage(eye.Rect.Size, rightEyeImg1.Depth, rightEyeImg1.NChannels);
+                            Cv.Copy(rightEyeImg1, rightEyeImg2, null);
+                            Cv.ResetImageROI(rightEyeImg1);
+
+                            
+                            Bitmap rightEyeBm = BitmapConverter.ToBitmap(rightEyeImg2);
+                            pctRightEye.Image = rightEyeBm;
+                           
+                                Console.Beep();
+                        }
+                        catch { }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            IplImage leftEyeImg1 = img.Clone();
+                            Cv.SetImageROI(leftEyeImg1, eye.Rect);
+                            IplImage leftEyeImg2 = Cv.CreateImage(eye.Rect.Size, leftEyeImg1.Depth, leftEyeImg1.NChannels);
+                            Cv.Copy(leftEyeImg1, leftEyeImg2, null);
+                            Cv.ResetImageROI(leftEyeImg1);
+
+                            Bitmap leftEyeBm = BitmapConverter.ToBitmap(leftEyeImg2);
+                            pctLeftEye.Image = leftEyeBm;
+                            
+                                Console.Beep();
+                        }catch{}
+                    }
+                }
+
+                Bitmap bm = BitmapConverter.ToBitmap(img);
+                bm.SetResolution(pctCvWindow.Width, pctCvWindow.Height);
+                pctCvWindow.Image = bm;
+
+                img = null;
+                bm = null;
+            }
+        }
+        #endregion
+
+        #region Form Handlers
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_cameraThread != null && _cameraThread.IsAlive)
+            {
+                _cameraThread.Abort();
+            }
+        }
+        #endregion
+
+        #region Button Handlers
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Text.Equals("Start"))
+            {
+                CaptureCamera();
+                btnStart.Text = "Stop";
+            }
+            else
+            {
+                _cameraThread.Abort();
+                btnStart.Text = "Start";
+            }
+        }
+
+        private void btnSaveImage_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = "Pravin's Eye detect.jpg";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String imageFileName = saveFileDialog1.FileName;
+                pctCvWindow.Image.Save(imageFileName);
+            }
+        }
+        #endregion
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pctLeftEye_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
